db6f14f47b5c09e7271314c1536f36cba4d8cb10
TanoshiiXD <rbharathsai@gmail.com>
2023-02-03 13:51:10 +0530

    Learning vectors

---
+++ vector.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int main() 
+{
+    int n;
+    vector<int> v;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+       {
+        v.push_back(i); 
+       }
+    for(auto i:v)
+       {
+        cout<<i<<" ";   
+       }   
+    cout<<endl;   
+    for(int i=0;i<n;i++)
+       {
+        int t;   
+        cin>>t;   
+        v.push_back(t); 
+       }
+    for(auto i:v)
+       {
+        cout<<i<<" ";   
+       }        
+    return 0;
+}
